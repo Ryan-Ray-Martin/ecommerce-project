3eec56f3d00f41e33b3b6212b39e6f3498b65e30
Ryan-Ray-Martin <ryanraymartin@gmail.com>
2019-12-17 01:50:47 -0800

    update menu items to navigate with router HOC

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 import './App.css';
 
+import {Switch, Route} from 'react-router-dom';
+
 import HomePage from "./components/homepage/homepage.component";
+
+const HatsPage =()=> (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+);
 
 function App() {
   return (
       <div>
-    <HomePage/>
-  </div>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/hats' component={HatsPage}/>
+          </Switch>
+      </div>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,11 +13,13 @@ class Directory extends React.Component {
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
+                linkUrl: 'hats'
             },
                 {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
+                    linkUrl: ''
 
                 },
                 {
@@ -31,6 +33,7 @@ class Directory extends React.Component {
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
+                    linkUrl: ''
 
                 },
                 {
@@ -38,6 +41,7 @@ class Directory extends React.Component {
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
+                    linkUrl: ''
                 }
             ]};
         }
@@ -46,8 +50,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             </div>
